e4e584f8e205f398edc946eb3dc5bbca37901683
senghoho <qisstun@naver.com>
2023-08-15 15:54:27 +0900

    Fix: 모델 필드 수정 및 이것저것...수정

--- Archive/urls.py
+++ Archive/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import BuddyListViewSet, RecordListViewSet, CompanionListViewSet, CardListViewSet, MyRecordViewSet
+from .views import RecordListViewSet, CompanionListViewSet, CardListViewSet, MyRecordViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "Archive"
 
 #1. 친구 목록 불러오는 기능
-BuddyList_router = routers.SimpleRouter()
-BuddyList_router.register("list", BuddyListViewSet, basename="list")
+
 
 #2. Record 스크랩 불러오는 기능
 RecordList_router = routers.SimpleRouter()
@@ -29,7 +28,6 @@ MyList_router.register("myrecord", MyRecordViewSet, basename="myrecord")
 
 
 urlpatterns = [
-    path("", include(BuddyList_router.urls)), #1. 친구 목록 불러오는 기능
     path("", include(RecordList_router.urls)), #2. Record 스크랩 불러오는 기능
     path("", include(CompanionList_router.urls)), #3. Companions 스크랩 불러오는 기능
     path("", include(CardList_router.urls)), #4. Card 스크랩 불러오는 기능
